3d2cbe7f063987c4f3ed95ddc07b888be76143dc
Unknown <smes3708@uni.sydney.edu.au>
2018-03-21 18:07:02 +1100

    wip: started Details page

---
+++ VideoStore.WebClient/Controllers/MediaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoStore.WebClient.ViewModels;
+using VideoStore.WebClient.ViewModels;
+
+
+namespace VideoStore.WebClient.Controllers
+{
+    public class MediaController : Controller
+    {
+        // GET: Media
+        public ActionResult Index()
+        {
+            return View("Details");
+        }
+        public ActionResult Details(int mediaId)
+        {
+            // get Media item
+            var media = ServiceFactory.Instance.CatalogueService.GetMediaById(mediaId);
+
+            return View(new MediaDetailsViewModel { Media = media});
+        }
+    }
+}
